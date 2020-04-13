168113e900f17dc415945e47aaf736416ef0b715
Nico Lauria <nflauria@outlook.com>
2020-04-13 14:05:03 -0700

    application controller auth methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  def require_no_user!
+    redirect_to dashboard_url(current_user) if current_user
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def login_user!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout_user!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def require_user!
+    redirect_to new_session_url if current_user.nil?
+  end
 end
