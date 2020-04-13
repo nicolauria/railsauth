fe7cdee7d86e1489d7e8a249b5bdf52e367cec8a
Nico Lauria <nflauria@outlook.com>
2020-04-13 14:10:56 -0700

    add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  root 'session#new'
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+  resource :dashboard, only: [:create, :show, :edit, :update]
 end
